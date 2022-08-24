354452ecbc8141d51ed086bce8f66171267a501f
trofimovpv <pashatrofimov@mail.ru>
2022-08-24 22:32:35 +0300

    Bonus

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+
+    public int calculate ( int cost)
+
+    {   int bonus = 20;
+        int price = cost;
+        int miles = price / bonus;
+
+        return miles;
+    }
+
+}
